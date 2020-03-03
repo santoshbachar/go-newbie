66167e2e88665335099bc9b8820243a77bc449b6
Santosh Bachar <santoshbachar@gmail.com>
2020-03-03 10:16:33 +0530

    first commit

---
+++ ifelse.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	earthIsFlat := false
+
+	// basic if/else
+	if earthIsFlat {
+		fmt.Println("You're crazy AF")
+	} else {
+		fmt.Println("Hmm... Good, you know the facts right !")
+	}
+
+	// if without else
+	if 2020%4 == 0 {
+		fmt.Println("2020 is a leap year")
+	}
+
+	// declare variable, then branch
+	// also mind 'else if'
+	if num := 9; num < 0 {
+		fmt.Println(num, " is negative")
+	} else if num < 10 {
+		fmt.Println(num, " has 1 digit")
+	} else {
+		fmt.Println(num, " has multiple digits")
+	}
+
+}
