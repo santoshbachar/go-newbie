ad74cb2e4e04fc929857bc1fb4526a98e0704f34
Santosh Bachar <santoshbachar@gmail.com>
2020-03-03 15:55:51 +0530

    first commit

---
+++ switch.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// comma for multiple expressions
+	// use of default
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch without expression
+	// non-constant case expressions
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// type switch compares types
+	varType := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Unknown type %T\n", t)
+		}
+	}
+
+	varType(true)
+	varType(1)
+	varType("go type check")
+}
