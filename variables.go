5d1ada783872f6f7b693a9a905e9656a85904105
Santosh Bachar <santoshbachar@gmail.com>
2020-03-03 09:30:07 +0530

    added variables.go

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var lang string = "go"
+	fmt.Println(lang)
+
+	var autoVar = 10
+	fmt.Println(autoVar)
+
+	var india, israel string = "Delhi", "Jerusalem"
+	fmt.Println(india, israel)
+
+	shortVar := "I love Go"
+	fmt.Println(shortVar)
+
+	var isGoFun = true
+	fmt.Println(isGoFun)
+
+}
