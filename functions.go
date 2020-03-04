ef1d0ddefb9543ba7ef84a1dc5a713c2387efa62
Santosh Bachar <santoshbachar@gmail.com>
2020-03-04 09:47:31 +0530

    multiple return values

--- functions.go
+++ functions.go
@@ -12,9 +12,20 @@ func add3(a, b, c int) int {
 	return a + b + c
 }
 
+func return2Val() (int, int) {
+	return 2, 3
+}
+
 func main() {
 	add := sum(1, 2)
 	fmt.Println("1+2 = ", add)
 
 	fmt.Println("1+2+3 = ", add3(1, 2, 3))
+
+	a, b := return2Val()
+	fmt.Println("return2Val() gives ", a, b)
+
+	// subset of return value
+	_, c := return2Val()
+	fmt.Println("only the second value is ", c)
 }
