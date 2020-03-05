ab345218bb478dfcba0a91c9813d9c717b1ed071
Santosh Bachar <santoshbachar@gmail.com>
2020-03-05 12:56:11 +0530

    add pointer functions

--- pointers.go
+++ pointers.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
+func zeroval(val int) {
+	val = 0
+}
+
+func zeroptr(val *int) {
+	*val = 0
+}
+
 func main() {
 	a := 2
 	pa := &a
 
 	fmt.Println("a=", a)
 	fmt.Println("pa=", pa)
+
+	i := 1
+	fmt.Println("Initial i value: ", i)
+
+	zeroval(i)
+	fmt.Println("after zeroval: ", i)
+
+	// & gives address of i
+	zeroptr(&i)
+	fmt.Println("after zeroptr: ", i)
 }
