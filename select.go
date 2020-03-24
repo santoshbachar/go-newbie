e68d84ad9c227749a917d96cc2dd69d79c652579
Santosh Bachar <santoshbachar@gmail.com>
2020-03-24 10:29:48 +0530

    fc

---
+++ select.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// ****************************************************
+// select lets you wait on multiple channel operations.
+// ****************************************************
+
+func main() {
+	c1 := make(chan string, 1)
+	c2 := make(chan string, 1)
+
+	// simulating blocking RPC operation, executing
+	// in concurrent goroutines.
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("c1 received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("c received", msg2)
+		}
+	}
+
+}
